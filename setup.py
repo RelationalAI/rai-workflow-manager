b270e8056c63da67b53d5508b2fb60b5f33726e8
Andrew Nepogoda <andrew.nepogoda@relational.ai>
2023-08-10 19:30:31 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+#
+# Copyright 2023 RelationalAI, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+from setuptools import setup
+
+import workflow
+
+setup(
+    author="RelationalAI, Inc.",
+    author_email="support@relational.ai",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    description="The RelationalAI Workflow Manager for batch runs",
+    install_requires=[
+        "ed25519==1.5",
+        "rai-sdk==0.6.14",
+        "protobuf==3.20.2",
+        "pyarrow==6.0.1",
+        "requests==2.31.0",
+        "requests-toolbelt==1.0.0",
+        "urllib3==1.26.6",
+        "azure-storage-blob==12.17.0"],
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    long_description="Enables access to the RelationalAI REST APIs from Python",
+    long_description_content_type="text/markdown",
+    name="rai-workflow-manager",
+    packages=["workflow"],
+    url="https://github.com/RelationalAI/rai-workflow-manager",
+    version=workflow.__version__)
