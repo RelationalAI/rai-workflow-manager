d8dc503a529fe360a3164a7524066121c99cbd8b
Andrew Nepogoda <andrew.nepogoda@relational.ai>
2023-10-20 14:02:30 +0200

    bump SDK

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         "tomli==2.0.1",
         "ed25519==1.5",
-        "rai-sdk==0.6.17",
+        "rai-sdk==0.6.18",
         "protobuf==3.20.3",
         "pyarrow==10.0.1",
         "requests==2.31.0",
