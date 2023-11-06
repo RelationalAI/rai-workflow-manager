9d36d189e575e77fc149d4780cff8b28167c76c7
Andrew Nepogoda <andrew.nepogoda@relational.ai>
2023-11-06 17:51:53 +0100

    Accumulate logs in a file so historic logs are easier to reference for comparisons

--- cli/logger.py
+++ cli/logger.py
@@ -1,7 +1,17 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler, RotatingFileHandler
+from workflow.common import BaseEnum
+
+log_filename = 'rwm.log'
 
 
-def configure(level=logging.INFO) -> logging.Logger:
+class LogRotationOption(str, BaseEnum):
+    DATE = 'date'
+    SIZE = 'size'
+
+
+def configure(level=logging.INFO, rotation: LogRotationOption = LogRotationOption.DATE,
+              log_file_size: int = 5) -> logging.Logger:
     # override default logging level for azure
     logger = logging.getLogger('azure.core')
     logger.setLevel(logging.ERROR)
@@ -10,8 +20,24 @@ def configure(level=logging.INFO) -> logging.Logger:
 
     logger = logging.getLogger()
     logger.setLevel(level)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('[%(asctime)s][%(levelname)s][%(name)s] %(message)s', "%H:%M:%S")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    formatter = logging.Formatter('[%(asctime)s.%(msecs)03d][%(levelname)s][%(name)s] %(message)s', '%Y-%m-%d %H:%M:%S')
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    if rotation == LogRotationOption.SIZE:
+        max_log_size = log_file_size * 1024 * 1024  # covert to Mb
+        size_rotation_handler = RotatingFileHandler(log_filename, maxBytes=max_log_size, backupCount=30,
+                                                    encoding='utf-8')
+        size_rotation_handler.setFormatter(formatter)
+        logger.addHandler(size_rotation_handler)
+        logger.info("Log rotation by size is enabled")
+    elif rotation == LogRotationOption.DATE:
+        time_rotation_handler = TimedRotatingFileHandler(log_filename, when='midnight', interval=1, backupCount=30,
+                                                         encoding='utf-8')
+        time_rotation_handler.setFormatter(formatter)
+        logger.addHandler(time_rotation_handler)
+        logger.info("Log rotation by date is enabled")
+
     return logger.getChild("cli")
