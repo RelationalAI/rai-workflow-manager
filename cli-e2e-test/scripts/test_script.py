137495fb25d2c8550a769a61560595831914d799
Andrew Nepogoda <andrew.nepogoda@relational.ai>
2023-11-22 12:39:08 +0100

    Update cli-e2e-test/scripts/test_script.py
    Co-authored-by: Stanislav Bedunkevich <63121780+stanislav-bedunkevich@users.noreply.github.com>

--- cli-e2e-test/scripts/test_script.py
+++ cli-e2e-test/scripts/test_script.py
@@ -7,4 +7,4 @@ formatter = logging.Formatter('[%(asctime)s.%(msecs)03d][%(levelname)s][%(name)s
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 my_logger = logger.getChild("my_test_logger")
-my_logger.info("Executed python and print message using logger")
+my_logger.info("Executed a python script to print a message using logger")
